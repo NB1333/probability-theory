eb7f3aa11e11f1a5ea2b48c9a54a9b07d56a0877
Nikita Bykov <91548015+NB1333@users.noreply.github.com>
2023-05-12 00:07:00 +0300

    ADDED: - project file

---
+++ main.py
@@ -0,0 +1,115 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import statistics as st
+
+mu = 0  # Mean
+sigma = 1.4  # Standard deviation
+n = 147  # Number of samples
+
+# Generate a list of random numbers
+frequency_list = np.random.normal(mu, sigma, n)
+
+sns.displot(frequency_list)
+
+plt.show()
+
+def plot_freq_polygon(freq_list):
+    # Create a list of x values
+    x = list(range(1, len(freq_list) + 1))
+
+    # Build a frequency polygon
+    plt.plot(x, freq_list, marker='o', linestyle='-')
+
+    # Set the x-axis and y-axis labels
+    plt.xlabel('Frequency number')
+    plt.ylabel('Frequency')
+    plt.title('Frequency polygon')
+
+    # Show the plot
+    plt.show()
+
+# Example usage
+plot_freq_polygon(frequency_list)
+
+def calculate_statistics(freq_list):
+    # Calculate the mean
+    mean = st.mean(freq_list)
+
+    # Calculate the median
+    median = st.median(freq_list)
+
+    # Calculate the mode
+    mode = st.mode(freq_list)
+
+    # Calculate the variance
+    variance = st.variance(freq_list)
+
+    # Calculate the standard deviation
+    stdev = st.stdev(freq_list)
+
+    # Show the results
+    print("MEAN:", mean)
+    print("MEDIAN:", median)
+    print("MODE:", mode)
+    print("VARIANCE:", variance)
+    print("STANDARD DEVIATION:", stdev)
+
+# Example usage
+calculate_statistics(frequency_list)
+
+def plot_boxplot(freq_list):
+    # Build a boxplot
+    plt.boxplot(freq_list)
+
+    # Set the x-axis and y-axis labels
+    plt.xlabel('Frequency')
+    plt.ylabel('Value')
+    plt.title('Boxplot')
+
+    # Show the plot
+    plt.show()
+
+def plot_pareto(freq_list):
+    # Sort the frequencies in descending order
+    freq_list.sort()
+    freq_list[:] = freq_list[::-1]
+
+    # Calculate the cumulative sum
+    cum_sum = [sum(freq_list[:i+1]) for i in range(len(freq_list))]
+
+    # Calculate the total sum
+    total_sum = sum(freq_list)
+
+    # Calculate the cumulative percentage
+    cum_percentage = [cum / total_sum * 100 for cum in cum_sum]
+
+    # Build a Pareto chart
+    fig, ax1 = plt.subplots()
+
+    ax1.bar(range(len(freq_list)), freq_list, align='center')
+    ax1.set_xlabel('Frequency number')
+    ax1.set_ylabel('Frequency')
+
+    ax2 = ax1.twinx()
+    ax2.plot(range(len(freq_list)), cum_percentage, color='r', marker='o')
+    ax2.set_ylabel('Cumulative %')
+
+    plt.title('Pareto chart')
+    plt.show()
+
+def plot_pie_chart(freq_list):
+    # Build a pie chart
+    plt.pie(freq_list, labels=range(1, len(freq_list)+1), autopct='%1.1f%%')
+
+    # Set the title
+    plt.title('Pie chart')
+
+    # Show the plot
+    plt.show()
+
+# Example usage
+
+plot_boxplot(frequency_list)
+plot_pareto(frequency_list)
+plot_pie_chart(frequency_list)
